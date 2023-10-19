191f38a4cdf4b2ab6488143b560749f06dbd6a0e
eregazzoni <emiliano.regazzoni@gmail.com>
2023-10-18 21:13:11 -0300

    checkout and recovery pass logic

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,14 @@ from .forms import RegistrationForms
 from .models import Account
 from django .contrib import messages, auth
 from django .contrib.auth.decorators import login_required
+from django .contrib.sites.shortcuts import get_current_site
+from django .template.loader import render_to_string
+
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+
+from django.utils.encoding import force_bytes
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -24,8 +32,23 @@ def register(request):
             user = Account.objects.create_user(first_name =first_name, last_name = last_name, email = email, username = username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            current_site = get_current_site(request)
+            mail_subject = 'Activación de cuenta en Lo de nestor'
+            body = render_to_string('accounts/account_verification_email.html',{
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+
+            to_email = email
+            send_email = EmailMessage(mail_subject,body, to=[to_email])
+            send_email.send()
+
+
             messages.success(request,'Se registro el usuario exitosamente')
-            return redirect('register')
+            return redirect('/accounts/login/?command=verification&email =' +email)
 
     
     context = {
@@ -43,7 +66,8 @@ def login(request):
         user = auth.authenticate(email=email, password = password)
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            messages.success(request, 'Has iniciado sesión correctamente')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Credenciales incorrectas')
             return redirect('login')
@@ -55,4 +79,86 @@ def logout(request):
 
     auth.logout(request)
     messages.success(request, 'Has cerrado sesión')
-    return redirect('login')
+    return redirect('login') 
+
+
+def activate(request, uidb64,token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk = uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Cuenta activada correctamente')
+        return redirect('login')
+    else:
+        messages.error(request, 'La activación ha fallado')
+        return redirect('register')
+
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email) #obtengo user si existe
+
+            current_site = get_current_site(request)
+            mail_subject = 'Cambio de password'
+            body = render_to_string('accounts/reset_password_email.html',{
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+
+            to_email = email
+            send_email = EmailMessage(mail_subject, body, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Un mail fue enviado a tu casilla para restablecer tu password')
+            return redirect('login')
+        
+        else:
+            messages.error(request, 'El mail ingresado no existe')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/forgotPassword.html')
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, user.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Por favor, restablece tu contraseña.')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'El enlace ha expirado.')
+        return redirect('login')
+        
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'El password se ha restablecido correctamente')
+            return redirect('login')
+        
+        else:
+            messages.error(request, 'Las password no coinciden, reintente')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
--- ecom/settings.py
+++ ecom/settings.py
@@ -137,6 +137,12 @@ MESSAGES_TAGS = {
     messages.ERROR : 'danger'
 }
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'emiregazzoni@gmail.com' #fernetcapote15
+EMAIL_HOST_PASSWORD = 'wdlx oekf zbqe awwd'
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
